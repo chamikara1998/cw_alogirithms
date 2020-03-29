830ea07b270b74e0f1a286e78d666699bbc5f1eb
chamikara samarasekara <chamikarasamarasekara@gmail.com>
2020-03-29 11:59:55 +0530

    WIP stage 3

--- src/main/java/components/Calculator.java
+++ src/main/java/components/Calculator.java
@@ -8,51 +8,48 @@ import util.OsDetector;
 
 import java.util.Scanner;
 
-public class Calculator implements Component{
+public class Calculator implements Component {
 
 
-    private void calculate(int vertices){
-        int graph[][] =new int[][] {
-                {0, 16, 13, 0, 0, 0},
-                {0, 0, 10, 12, 0, 0},
-                {0, 4, 0, 0, 14, 0},
-                {0, 0, 9, 0, 0, 20},
-                {0, 0, 0, 7, 0, 4},
-                {0, 0, 0, 0, 0, 0}
-        };
+    private void calculate(int vertices,int[][] dataStructure) {
         MaximumFlowProcessor maximumFlowProcessor = new MaximumFlowProcessor(vertices);
 
         System.out.println("The maximum possible flow is " +
-                maximumFlowProcessor.getMaxFlow(graph, 0, 5));
+                maximumFlowProcessor.getMaxFlow(dataStructure, 0, 5));
     }
 
     @Override
     public void render() {
         int vertices = getVertices();
         int[][] dataStructure = getDataStructure(vertices);
-        calculate(vertices);
+        calculate(vertices,dataStructure);
     }
 
     private int[][] getDataStructure(int vertices) {
         System.out.println();
         System.out.println("Enter Data Structure");
+        System.out.println("Put spaces between two data points");
         Scanner scanner = new Scanner(System.in);
-        for (int i = 0;i<vertices;i++){
-            scanner.nextInt();
-            System.out.print(" | ");
+        int[][] dataStructure = new int[vertices][vertices];
+        for (int i = 0; i < vertices; i++) {
+            String inputData = scanner.nextLine();
+            String[] dataRowStrings = inputData.split(" ");
+            for (int r = 0; r < vertices; r++) {
+                dataStructure[i][r] = Integer.parseInt(dataRowStrings[r]);
+            }
         }
-        return null;
+        return dataStructure;
     }
 
-    private int getVertices(){
+    private int getVertices() {
         int vertices;
         Scanner input = new Scanner(System.in);
         System.out.print("Input number of nodes -> ");
-        if(OsDetector.detect() == Os.linux){
+        if (OsDetector.detect() == Os.linux) {
             System.out.print(ConsoleColors.GREEN);
         }
         vertices = input.nextInt();
-        if(OsDetector.detect() == Os.linux){
+        if (OsDetector.detect() == Os.linux) {
             System.out.print(ConsoleColors.RESET);
         }
         return vertices;
